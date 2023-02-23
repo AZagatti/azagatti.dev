694fc2213b449053378e77b0efe216d85d5ba385
azagatti <andre.zagatti1@gmail.com>
2023-02-23 20:38:33 -0300

    feat: add 404 and redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://azagatti.dev',
   markdown: {
     remarkPlugins: [remarkReadingTime],
     syntaxHighlight: 'prism',

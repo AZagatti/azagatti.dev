986af55b104f5fc792ffa08a01d2d59e70b54691
azagatti <andre.zagatti1@gmail.com>
2023-02-28 19:04:58 -0300

    feat: add px-8 to post

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -7,7 +7,7 @@ interface PostProps {
 
 export const Post = ({ children, post }: PostProps) => {
   return (
-    <article className="max-w-6xl px-2 md:px-8">
+    <article className="max-w-6xl px-8">
       <div className="max-w-full">
         <h2 className="text-3xl fontbold">{post.title}</h2>
         <p className="text-lg font-normal pb-8 pt-2 text-gray-500 dark:text-gray-300">{`${formatDate(

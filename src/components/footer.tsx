195cc5ed8d96996f98373618230db2ce7ff9aa9c
azagatti <andre.zagatti1@gmail.com>
2023-06-13 21:46:12 -0300

    feat: add lead forms

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,22 +3,101 @@ import { useTranslation } from 'react-i18next'
 import { GithubIcon } from './icons/github'
 import { LinkedinIcon } from './icons/linkedin'
 import { TwitterIcon } from './icons/twitter'
+import { YoutubeIcon } from './icons/youtube'
+import { InstagramIcon } from './icons/instagram'
 
 export const Footer = () => {
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
 
   return (
     <>
       <hr className="my-8" />
+
       <div className="w-full flex items-center justify-center">
         <div className="md:max-w-6xl px-4 xl:px-0 flex-1">
+          {i18n.language !== 'en' ? (
+            <div className="flex gap-8 justify-between flex-col md:flex-row mb-4">
+              <div className="flex-1">
+                <form
+                  klicksend-form-id="aauBz57"
+                  method="post"
+                  action="//handler.klicksend.com.br/subscription/aauBz57"
+                >
+                  <h2 className="mb-2 text-lg font-bold">
+                    Se inscreva para novidades:
+                  </h2>
+                  <input
+                    className="mb-2 p-2 rounded-md border-[1px] border-gray-600 dark:border-gray-100 md:w-80"
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email"
+                    required
+                  />
+                  <div klicksend-gdpr-text="">
+                    <p className="text-xs">
+                      Para mais informações, acesse:
+                      <a
+                        href="https://klickpages.com.br/politica-de-privacidade/"
+                        target="_blank"
+                        rel="noopener"
+                        className="ml-1 dark:text-blue-400 text-blue-800"
+                      >
+                        Política de Privacidade
+                      </a>
+                    </p>
+                  </div>
+                  <div>
+                    <div className="mt-2 mb-4">
+                      <input
+                        type="checkbox"
+                        name="gdpr"
+                        id="gdpr"
+                        placeholder="Concordo em receber os e-mails"
+                        defaultValue="Concordo em receber os e-mails"
+                        required
+                      />
+                      <label
+                        htmlFor="gdpr"
+                        className="dark:text-gray-200 text-gray-900 ml-2"
+                      >
+                        Concordo em receber os e-mails
+                      </label>
+                    </div>
+                    <div
+                      style={{
+                        position: 'absolute',
+                        left: -5000,
+                      }}
+                      aria-hidden
+                    >
+                      <input type="text" name="b_aauBz57" tabIndex={-1} />
+                    </div>
+                  </div>
+                  <button className="py-2 px-8 bg-gray-800 dark:bg-gray-100 rounded-md text-gray-200 dark:text-gray-900 hover:opacity-90 transition-opacity">
+                    Enviar
+                  </button>
+                </form>
+              </div>
+              <div className="flex-1 flex md:justify-end">
+                <div>
+                  <img
+                    src="/img/logo.png"
+                    alt="A. Zagatti Logo"
+                    width="200"
+                    height="56"
+                  />
+                </div>
+              </div>
+            </div>
+          ) : null}
           <div className="flex gap-8 justify-between flex-col md:flex-row">
             <div className="flex-1">
               <h4 className="text-xl mb-8">{t('name')}</h4>
               <p className="text-lg">{t('footer.description')}</p>
             </div>
             <div className="flex-1">
-              <div className="flex md:justify-end mb-8">
+              <div className=" flex md:justify-end">
                 <a
                   className="md:ml-8 text-2xl no-underline"
                   target="_blank"
@@ -41,20 +120,40 @@ export const Footer = () => {
                   className="ml-8 text-2xl no-underline"
                   target="_blank"
                   rel="noreferrer noopener"
-                  href="https://twitter.com/andre-zagatti/"
+                  href="https://twitter.com/andre_zagatti/"
                   aria-label={t('footer.aria.twitter')!}
                 >
                   <TwitterIcon />
                 </a>
+                <a
+                  className="ml-8 text-2xl no-underline"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  href="https://www.youtube.com/@AZagatti"
+                  aria-label={t('footer.aria.youtube')!}
+                >
+                  <YoutubeIcon />
+                </a>
+                <a
+                  className="ml-8 text-2xl no-underline"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  href="https://www.instagram.com/reactwayjs"
+                  aria-label={t('footer.aria.instagram')!}
+                >
+                  <InstagramIcon />
+                </a>
               </div>
-              <div className="flex md:justify-end">
-                <img
-                  src="/img/logo.png"
-                  alt="A. Zagatti Logo"
-                  width="200"
-                  height="56"
-                />
-              </div>
+              {i18n.language !== 'pt' ? (
+                <div className="flex md:justify-end mt-4">
+                  <img
+                    src="/img/logo.png"
+                    alt="A. Zagatti Logo"
+                    width="200"
+                    height="56"
+                  />
+                </div>
+              ) : null}
             </div>
           </div>
           <p className="mt-16 mb-8 text-base">
---
+++ src/components/icons/youtube.tsx
@@ -0,0 +1,28 @@
+export const YoutubeIcon = () => {
+  return (
+    <svg
+      height="24"
+      width="24"
+      version="1.1"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 461.001 461.001"
+      fill="#000000"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <g>
+          <path
+            className="fill-black dark:fill-white"
+            d="M365.257,67.393H95.744C42.866,67.393,0,110.259,0,163.137v134.728 c0,52.878,42.866,95.744,95.744,95.744h269.513c52.878,0,95.744-42.866,95.744-95.744V163.137 C461.001,110.259,418.135,67.393,365.257,67.393z M300.506,237.056l-126.06,60.123c-3.359,1.602-7.239-0.847-7.239-4.568V168.607 c0-3.774,3.982-6.22,7.348-4.514l126.06,63.881C304.363,229.873,304.298,235.248,300.506,237.056z"
+          ></path>
+        </g>
+      </g>
+    </svg>
+  )
+}

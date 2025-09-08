bb6874369ece0f6069a2672c769b81ad0c50aa4d
azagatti <andre.zagatti1@gmail.com>
2025-09-08 10:18:12 -0300

    chore: lint

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'André Zagatti';
-export const SITE_DESCRIPTION = '💯';
+export const SITE_TITLE = 'André Zagatti'
+export const SITE_DESCRIPTION = '💯'
